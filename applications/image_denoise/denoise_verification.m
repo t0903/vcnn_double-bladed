1f71e1320a4d105e1f35607e02c808e2363e2e5c
Jimmy Ren <jimmy.sj.ren@gmail.com>
2015-01-23 00:39:07 +0800

    Updated the verification script of image denoise example.

--- applications/image_denoise/denoise_verification.m
+++ applications/image_denoise/denoise_verification.m
@@ -16,37 +16,20 @@ addpath data/MNIST/
 clearvars -global config;
 clearvars -global mem;
 
+global config;
 denoise_configure();
 init(0);
-global config;
 
 % for gradient checking
-
-%image = config.NEW_MEM(ones(64, 64, 3, config.batch_size));
-%label = config.NEW_MEM(ones(56, 56, 3, config.batch_size));
-
-img = im2double(imread('C:\Info\Jimmy\code\VCNN_improc\data\input\blurred_images\non-saturated_disk7\1.png'));
+img = im2double(imread('sample1.png'));
 startpt = 150;
-image = config.NEW_MEM((img(startpt:startpt+64-1, startpt:startpt+64-1, :)));
-label = config.NEW_MEM((image(1:56, 1:56, :)));
+image = config.NEW_MEM((img(startpt:startpt+config.input_size(1)-1, startpt:startpt+config.input_size(2)-1, :)));
+label = config.NEW_MEM((image(1:config.output_size(1), 1:config.output_size(2), :)));
 
 image = repmat(image, 1,1,1,config.batch_size);
 label = repmat(label, 1,1,1,config.batch_size);
 
 op_train_pipe(image, label);
 computeNumericalGradient(image, label, 1);
-
-%{
-% for speed testing
-I = gpuArray(single(rand(32,32,1,config.batch_size)));
-y = gpuArray(single(rand(10, config.batch_size)));
-loop = 100;
-tic
-for m = 1:loop
-op_train_pipe(I, y);
-end
-toc/loop/config.batch_size
-
-%}
 
 
