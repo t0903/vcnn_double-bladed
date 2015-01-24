ac3789f596378489bfb6715c2c09d1f71d92f086
Jimmy Ren <jimmy.sj.ren@gmail.com>
2015-01-24 11:26:35 +0800

    Revise the denoise example.

--- applications/image_denoise/denoise_test_demo.m
+++ applications/image_denoise/denoise_test_demo.m
@@ -19,9 +19,9 @@ clear;
 I = im2double(imread('sample2.png'));
 global config;
 denoise_configure();
-prepare_net(size(I, 1), size(I, 2), 'epoc11.mat');
+prepare_net(size(I, 1), size(I, 2), 'w_gaussian.mat');
 final_output = apply_net(config.NEW_MEM(I));
 final_output = gather(final_output);
-imshow([I final_output]);
-
+figure, imshow([I final_output]);
+drawnow();
 
