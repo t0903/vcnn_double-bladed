c3f8067b4561b7139f8cb527be8372b5ede6ad65
Jimmy <jimmy.sj.ren@gmail.com>
2015-02-10 10:25:06 +0800

    Fixed a bug. Not using gpuArray explicitly.

--- layers/dropout_forward.m
+++ layers/dropout_forward.m
@@ -2,7 +2,7 @@ function dropout_forward()
     global config mem;
     curr_layer_idx = config.misc.current_layer - 1;
     if config.misc.training;
-        dm = gpuArray(single(uint8(rand(size(mem.activations{curr_layer_idx})))));
+        dm = config.NEW_MEM(single(uint8(rand(size(mem.activations{curr_layer_idx})))));
         mem.activations{curr_layer_idx} = mem.activations{curr_layer_idx} .* dm;
     else
         mem.activations{curr_layer_idx} = mem.activations{curr_layer_idx} ./ 2;
